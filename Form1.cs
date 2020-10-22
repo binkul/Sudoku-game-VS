efece643a58954cc2bbf7318e53526c14b8ac9f3
binkul <jacekbinkul@gmail.com>
2020-10-22 20:47:04 +0200

    Initial commit

--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Sudoku
 {
-    public partial class Form1 : Form
+    public partial class FormSudoku : Form
     {
-        public Form1()
+        public FormSudoku()
         {
             InitializeComponent();
         }
---
+++ Sudoku/Element/Position.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku.Sudoku.Element
+{
+    class Position
+    {
+        private int x { get; set; }
+        private int y { get; set; }
+
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Position position &&
+                   x == position.x &&
+                   y == position.y;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1502939027;
+            hashCode = hashCode * -1521134295 + x.GetHashCode();
+            hashCode = hashCode * -1521134295 + y.GetHashCode();
+            return hashCode;
+        }
+    }
+}
